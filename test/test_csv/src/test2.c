1e687eb142965d6cd03d22bb8bae3480b00912e9
Matteo Civiero <matteo.civiero@studenti.unipd.it>
2022-07-10 17:23:09 +0200

    test2.c aggiornato con test sulle caselle vuote

--- test/test_csv/src/test2.c
+++ test/test_csv/src/test2.c
@@ -9,6 +9,7 @@ int main()
 	int ret;
 	char *nomefile = "../../../test/test_csv/files/file1.csv";
 	char **fields;
+	
 	out = csv_open(&file, nomefile, ',', 4);
 	csv_error_string(out, &error);
 	printf("%s\n", error);
@@ -58,5 +59,23 @@ int main()
 	csv_error_string(ret, &error);
 	printf("%s\n", error);
 	csv_close(&file);
+	
+	nomefile = "../../../test/test_csv/files/file4.csv";
+	out = csv_open(&file, nomefile, ',', 4);
+	csv_error_string(out, &error);
+	printf("%s\n", error);
+	while((ret = csv_read_record(&file, &fields)) == CSV_OK){
+		csv_error_string(ret, &error);
+		printf("%s\n", error);
+		printf("%s\t", fields[0]);
+		printf("%s\t", fields[1]);
+		printf("%s\t", fields[2]);
+		printf("%s\t", fields[3]);
+		printf("\n");
+	}
+	csv_error_string(ret, &error);
+	printf("%s\n", error);
+	csv_close(&file);
+	
 	return 0;
 }
