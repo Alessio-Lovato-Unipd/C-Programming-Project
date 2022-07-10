c17a2fb564bfd76b0150b144432fee86027f8607
Matteo Civiero <matteo.civiero@studenti.unipd.it>
2022-07-10 15:48:28 +0200

    testing non ancora concluso

--- test/test_csv/src/test2.c
+++ test/test_csv/src/test2.c
@@ -7,21 +7,28 @@ int main()
 	int out = 0;
 	char *error;
 	char *nomefile = "../../../test/test_csv/files/file1.csv";
+	char **fields;
 	out = csv_open(&file, nomefile, ',', 4);
+	while(csv_read_record(&file, &fields) == CSV_OK){
+		printf("%s\n", fields[0]);
+		printf("%s\n", fields[1]);
+		printf("%s\n", fields[2]);
+		printf("%s\n", fields[3]);
+	}
 	csv_error_string(out, &error);
 	printf("%s\n", error);
 	csv_close(&file);
 	
-	nomefile = "../../../test/test_csv/files/file2.csv";
+	/*nomefile = "../../../test/test_csv/files/file2.csv";
 	out = csv_open(&file, nomefile, ',', 5);
 	csv_error_string(out, &error);
 	printf("%s\n", error);
 	csv_close(&file);
 	
-	nomefile = "../../../test/test_csv/files/file3.csv"
+	nomefile = "../../../test/test_csv/files/file3.csv";
 	out = csv_open(&file, nomefile, ',', 5);
 	csv_error_string(out, &error);
 	printf("%s\n", error);
-	csv_close(&file);
+	csv_close(&file);*/
 	return 0;
 }
