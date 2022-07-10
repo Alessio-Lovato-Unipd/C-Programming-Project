125e8373603900fa622352ef0dea182c1df4f7b9
Matteo Civiero <matteo.civiero@studenti.unipd.it>
2022-07-10 17:12:36 +0200

    testing lettura completato

--- test/test_csv/src/test2.c
+++ test/test_csv/src/test2.c
@@ -6,29 +6,57 @@ int main()
 	struct csv file;
 	int out = 0;
 	char *error;
+	int ret;
 	char *nomefile = "../../../test/test_csv/files/file1.csv";
 	char **fields;
 	out = csv_open(&file, nomefile, ',', 4);
-	while(csv_read_record(&file, &fields) == CSV_OK){
-		printf("%s\n", fields[0]);
-		printf("%s\n", fields[1]);
-		printf("%s\n", fields[2]);
-		printf("%s\n", fields[3]);
-	}
 	csv_error_string(out, &error);
+	printf("%s\n", error);
+	while((ret = csv_read_record(&file, &fields)) == CSV_OK){
+		csv_error_string(ret, &error);
+		printf("%s\n", error);
+		printf("%s\t", fields[0]);
+		printf("%s\t", fields[1]);
+		printf("%s\t", fields[2]);
+		printf("%s\t", fields[3]);
+		printf("\n");
+	}
+	csv_error_string(ret, &error);
 	printf("%s\n", error);
 	csv_close(&file);
 	
-	/*nomefile = "../../../test/test_csv/files/file2.csv";
+	nomefile = "../../../test/test_csv/files/file2.csv";
 	out = csv_open(&file, nomefile, ',', 5);
 	csv_error_string(out, &error);
+	while((ret = csv_read_record(&file, &fields)) == CSV_OK){
+		csv_error_string(ret, &error);
+		printf("%s\n", error);
+		printf("%s\t", fields[0]);
+		printf("%s\t", fields[1]);
+		printf("%s\t", fields[2]);
+		printf("%s\t", fields[3]);
+		printf("%s\t", fields[4]);
+		printf("\n");
+	}
+	csv_error_string(ret, &error);
 	printf("%s\n", error);
 	csv_close(&file);
 	
 	nomefile = "../../../test/test_csv/files/file3.csv";
 	out = csv_open(&file, nomefile, ',', 5);
 	csv_error_string(out, &error);
+	while((ret = csv_read_record(&file, &fields)) == CSV_OK){
+		csv_error_string(ret, &error);
+		printf("%s\n", error);
+		printf("%s\t", fields[0]);
+		printf("%s\t", fields[1]);
+		printf("%s\t", fields[2]);
+		printf("%s\t", fields[3]);
+		printf("%s\t", fields[4]);
+		printf("\n");
+	}
+	csv_error_string(ret, &error);
 	printf("%s\n", error);
-	csv_close(&file);*/
+	csv_close(&file);
 	return 0;
 }
