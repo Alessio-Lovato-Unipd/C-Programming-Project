aebe9ac6e6cbe9f854d9ac3aecba5f58ccd769f2
Matteo Civiero <matteo.civiero@studenti.unipd.it>
2022-07-12 15:13:30 +0200

    aggiustamento file di testing

--- test/test_csv/src/test2.c
+++ test/test_csv/src/test2.c
@@ -1,6 +1,10 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include "csv.h"
+/*************************************************************************
+***** TESTING LIBRERIA CSV CON FILE CONTENENTI DIVERSI INPUT *************
+*************************************************************************/
+
 int main()
 {
 	struct csv file;
@@ -10,6 +14,7 @@ int main()
 	char *nomefile = "../../../test/test_csv/files/file1.csv";
 	char **fields;
 	
+	//testing file1
 	out = csv_open(&file, nomefile, ',', 4);
 	csv_error_string(out, &error);
 	printf("%s\n", error);
@@ -25,7 +30,9 @@ int main()
 	csv_error_string(ret, &error);
 	printf("%s\n", error);
 	csv_close(&file);
+	printf("test file 1 concluso\n\n");
 	
+	//testing file2
 	nomefile = "../../../test/test_csv/files/file2.csv";
 	out = csv_open(&file, nomefile, ',', 5);
 	csv_error_string(out, &error);
@@ -42,9 +49,11 @@ int main()
 	csv_error_string(ret, &error);
 	printf("%s\n", error);
 	csv_close(&file);
+	printf("test file 2 concluso\n\n");
 	
+	//testing file3
 	nomefile = "../../../test/test_csv/files/file3.csv";
-	out = csv_open(&file, nomefile, ',', 5);
+	out = csv_open(&file, nomefile, ',', 6);
 	csv_error_string(out, &error);
 	while((ret = csv_read_record(&file, &fields)) == CSV_OK){
 		csv_error_string(ret, &error);
@@ -59,7 +68,9 @@ int main()
 	csv_error_string(ret, &error);
 	printf("%s\n", error);
 	csv_close(&file);
+	printf("test file 3 concluso\n\n");
 	
+	//testing file4
 	nomefile = "../../../test/test_csv/files/file4.csv";
 	out = csv_open(&file, nomefile, ',', 4);
 	csv_error_string(out, &error);
@@ -76,6 +87,7 @@ int main()
 	csv_error_string(ret, &error);
 	printf("%s\n", error);
 	csv_close(&file);
+	printf("test file 4 concluso\n\n");
 	
 	return 0;
 }
