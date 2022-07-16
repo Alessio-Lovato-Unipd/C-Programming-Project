3058c489415581e6bba9713d577af59d1d3953d7
Alberto Piccina <alberto.piccina@studenti.unipd.it>
2022-07-16 11:15:31 +0200

    Sono state implementate tutte le funzioni relative alla gestione del file weather.csv, sul main è stata predisposta una porzione di codice dedicata alla verifica e alla stampa dei dati di weather.

--- lib/estrazione_csv/include/estrattore_csv.h
+++ lib/estrazione_csv/include/estrattore_csv.h
@@ -15,13 +15,13 @@
     struct weather {
         char *nome;
         float pressione;
-        float temperatura1;
-        float velocità_vento1;
-        float rugosità;
-        float temperatura2;
-        float velocità_vento2;
+        float temperatura1; //ad altezza=2m
+        float velocita_vento1; //ad altezza=10m
+        float rugosita;
+        float temperatura2; //ad altezza=10m
+        float velocita_vento2; //ad altezza=80m
         struct weather *prev;
-    }
+    };
 
 
     //funzione per estrarre i dati dal file turbine_data.csv
@@ -30,7 +30,7 @@
     struct turbina *estrazione_dati_turbine(struct turbina *puntatore, char *const percorso_file_turbine_data, int *errore);
 
     // creazione nuovo elemento lista turbina
-    struct turbina *nuovo_elemento(struct turbina *elemento_attuale, char **fields);
+    struct turbina *nuovo_elemento_turbina(struct turbina *elemento_attuale_turbina, char **fields);
 
     //funzione per liberare la memoria heap allocata con la lista
     struct turbina *svuota_lista_turbine_data(struct turbina *head_turbina);
@@ -39,6 +39,12 @@
     struct turbina *cerca_dati_turbina(char *nome_modello_turbina,  const struct turbina *head_turbina);
 
     struct weather *estrazione_dati_weather(struct weather *puntatore, char *const percorso_file_weather, int *errore);
+
+    struct weather *nuovo_elemento_weather(struct weather *elemento_attuale_weather, char** fields);
+
+    struct weather *svuota_lista_weather(struct weather *head_weather);
+
+    struct weather *cerca_dati_weather(char *giorno_orario_weather, const struct weather *head_weather);
 
 
 #endif
