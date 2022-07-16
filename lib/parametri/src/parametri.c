6f64ccb7db74eac1332dc2a6fe155ee8452cabed
Edoardo Menegotto <edoardo.menegotto@studenti.unipd.it>
2022-07-16 15:56:57 +0200

    ciclo for al posto del while

--- lib/parametri/src/parametri.c
+++ lib/parametri/src/parametri.c
@@ -14,10 +14,14 @@ struct parametro *aggiungi_elemento(const struct parametro *elemento_attuale, fl
 		exit(EXIT_FAILURE);
     }
 
+    //salvo i dati
     nuovo_elemento->velocita_vento = val_vento;
     nuovo_elemento->temperatura_aria = val_temperatura;
     nuovo_elemento->densita_aria = val_densita;
+    //salvo posizione 
     nuovo_elemento->next = elemento_attuale; 
+
+    return nuovo_elemento;
 }
 
 /**************************GESTIONE DEI CALCOLI********/
@@ -52,12 +56,12 @@ float calcolo_densita_aria(float altezza1, float pressione1, float altezza2, flo
 /******************CALCOLO DEI PARAMETRI******************/
 void calcolo_parametri(const struct weather *in, const struct altezze  *h, float altezza_mozzo, struct parametro *out)
 {
-
+    struct weather *p; 
     float vento, temperatura, pressione, densita;
     //Chiudo quali metodi di calcolo vuole utilizzare per ognuno dei 3 valori da calcolare
 
     //Calcolo tutti i parametri a partire dai dati weather
-    while(in != NULL){
+    for(p = in; p != NULL; p = p->prev) {
         //calcolo i 3 parametri
         vento = calcolo_vel_vento(0, h->h_vel1, in->velocità_vento1, h->h_vel2, in->velocità_vento2, in->rugosità, altezza_mozzo);
         temperatura = calcolo_temperatura_aria(h->h_t1, in->temperatura1, h->h_t2, in->temperatura2, altezza_mozzo);
@@ -65,10 +69,6 @@ void calcolo_parametri(const struct weather *in, const struct altezze  *h, float
         
         //salvo i 3 parametri calcolati nell'elemento corrente
         aggiungi_elemento(out, vento, temperatura, densita);
-
-        //aggiorno i punattori all'elemento successivo
-        in = in->prev;
-        out = out->next;
     }
     
 }
