2d88d13025a348c40429e41e050da0af4aa9b93d
Edoardo Menegotto <edoardo.menegotto@studenti.unipd.it>
2022-07-16 09:43:40 +0200

    switch per seezionare metodo di calcolo

--- lib/parametri/include/parametri.h
+++ lib/parametri/include/parametri.h
@@ -1,6 +1,6 @@
 #ifndef PARAMETRI_H
     #define PARAMETRI_H
-    enum tipo_calcolo_vento {INTERPOLAZIONE_LINEARE, INTERPOLAZIONE_LOGARITMICA, PROFILO_LOGARITMICO, HELLMAN};
+    typedef enum tipo_calcolo_vento {INTERPOLAZIONE_LINEARE, INTERPOLAZIONE_LOGARITMICA, PROFILO_LOGARITMICO, HELLMAN};
     struct parametro{
         float velocita_vento;
         float temperatura_aria;
@@ -8,5 +8,5 @@
         struct parametro *prev;
     }
 
-    void calcolo_parametri(const struct weather *in, float altezza_mozzo, struct parametro *out);
+    void calcolo_parametri(const struct weather *in, const struct altezze  *h, float altezza_mozzo, struct parametro *out);
 #endif
--- lib/parametri/src/parametri.c
+++ lib/parametri/src/parametri.c
@@ -2,9 +2,18 @@
 #include "../../formule/include/formule.h"
 #include "../../estrazione_csv/include/estrattore_csv.h"
 
-float calcolo_vel_vento(float altezza1, float velocita1, float altezza2, float velocita2, float rugosita, float altezza_x)
+float calcolo_vel_vento(enum tipo_calcolo_vento metodo, float altezza1, float velocita1, float altezza2, float velocita2, float rugosita, float altezza_x)
 {
-    return interpolazione_lineare(altezza1, velocita1, altezza2, velocita1, altezza_x);
+    switch (metodo)
+    {
+    case INTERPOLAZIONE_LINEARE:
+        return interpolazione_lineare(altezza1, velocita1, altezza2, velocita1, altezza_x);
+        break;
+    
+    default:
+        break;
+    }
+    
 }
 
 float calcolo_temperatura_aria(float altezza1, float temperatura1, float altezza2, float temperatura2, float altezza_x)
