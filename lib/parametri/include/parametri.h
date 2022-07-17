d6f7b800ed1244b2ba04410eff934bce2c816704
Edoardo Menegotto <edoardo.menegotto@studenti.unipd.it>
2022-07-17 14:57:13 +0200

    aggiunta degli switch

--- lib/parametri/include/parametri.h
+++ lib/parametri/include/parametri.h
@@ -1,12 +1,16 @@
 #ifndef PARAMETRI_H
     #define PARAMETRI_H
     typedef enum tipo_calcolo_vento {INTERPOLAZIONE_LINEARE, INTERPOLAZIONE_LOGARITMICA, PROFILO_LOGARITMICO, HELLMAN};
+    typedef enum tipo_calcolo_temperatura {INTERPOLAZIONE_LINEARE, GRADIENTE_LINEARE};
+    typedef enum tipo_calcolo_densita {BAROMETRICO, GAS_IDEALE};
+
     struct parametro{
         float velocita_vento;
         float temperatura_aria;
         float densita_aria;
         struct parametro *next;
-    }
+    };
 
-    void calcolo_parametri(const struct weather *in, const struct altezze  *h, float altezza_mozzo, struct parametro *out);
+    float calcolo_vel_vento(enum tipo_calcolo_vento metodo, float altezza1, float velocita1, float altezza2, float velocita2, float rugosita, float h_ostacolo,float altezza_x);
+    
 #endif
--- lib/parametri/src/parametri.c
+++ lib/parametri/src/parametri.c
@@ -1,6 +1,7 @@
-#include "../include/parametri.h"
-#include "../../formule/include/formule.h"
-#include "../../estrazione_csv/include/estrattore_csv.h"
+
+#include "parametri.h"
+#include "formule.h"
+#include "estrattore_csv.h"
 
 /************************GESTIONE STRUTTURA PARAMETRI********************/
 
@@ -24,8 +25,8 @@ struct parametro *aggiungi_elemento(const struct parametro *elemento_attuale, fl
     return nuovo_elemento;
 }
 
-/**************************GESTIONE DEI CALCOLI********/
-float calcolo_vel_vento(enum tipo_calcolo_vento metodo, float altezza1, float velocita1, float altezza2, float velocita2, float rugosita, float altezza_x)
+/**************************GESTIONE DEI CALCOLI*****************/
+float calcolo_vel_vento(enum tipo_calcolo_vento metodo, float altezza1, float velocita1, float altezza2, float velocita2, float rugosita, float h_ostacolo,float altezza_x)
 {
     switch (metodo)
     {
@@ -33,20 +34,59 @@ float calcolo_vel_vento(enum tipo_calcolo_vento metodo, float altezza1, float ve
         return interpolazione_lineare(altezza1, velocita1, altezza2, velocita1, altezza_x);
         break;
     
+    case INTERPOLAZIONE_LOGARITMICA:
+        return interpolazione_logaritmica(altezza1, velocita1, altezza2, velocita2, altezza_x);
+        break;
+
+    case PROFILO_LOGARITMICO:
+        return profilo_logaritmico(altezza1, velocita1, rugosita, h_ostacolo, altezza_x);//aggiungere lunghezza ostacolo
+        break;
+
+    case HELLMAN: 
+        return hellman(altezza1, velocita1, rugosita, altezza_x);
+        break;
+
     default:
+        return interpolazione_lineare(altezza1, velocita1, altezza2, velocita1, altezza_x);
         break;
     }
     
 }
 
-float calcolo_temperatura_aria(float altezza1, float temperatura1, float altezza2, float temperatura2, float altezza_x)
+float calcolo_temperatura_aria(enum tipo_calcolo_temperatura metodo, float altezza1, float temperatura1, float altezza2, float temperatura2, float altezza_x)
 {
-    return interpolazione_lineare(altezza1, temperatura1, altezza2, temperatura2, altezza_x);
+    switch (metodo)
+    {
+    case INTERPOLAZIONE_LINEARE:
+        return interpolazione_lineare(altezza1, temperatura1, altezza2, temperatura2, altezza_x);
+        break;
+    
+    case GRADIENTE_LINEARE:
+        return gradiente_lineare(altezza1, temperatura1, altezza_x);
+        break;
+    default:
+        return interpolazione_lineare(altezza1, temperatura1, altezza2, temperatura2, altezza_x);
+        break;
+    }
+    
 }
 
-float calcolo_densita_aria(float altezza1, float pressione1, float altezza2, float pressione2, float altezza_x)
+float calcolo_densita_aria(enum tipo_calcolo_densita metodo, float altezza1, float pressione1, float temperatura_ambiente,float temperatura_x,float altezza_x)
 {
-    return interpolazione_lineare(altezza1, pressione1, altezza2, pressione2, altezza_x);
+    switch (metodo)
+    {
+    case BAROMETRICO:
+        return barometrico(altezza1, pressione1, temperatura_ambiente, temperatura_x, altezza_x);
+        break;
+    
+    case GAS_IDEALE:
+        return gas_ideale(altezza1, pressione1, temperatura_x, altezza_x);
+        break;
+
+    default:
+        return gas_ideale(altezza1, pressione1, temperatura_x, altezza_x);
+        break;
+    }
 }
 
 
@@ -58,15 +98,19 @@ void calcolo_parametri(const struct weather *in, const struct altezze  *h, float
 {
     struct weather *p; 
     float vento, temperatura, pressione, densita;
-    //Chiudo quali metodi di calcolo vuole utilizzare per ognuno dei 3 valori da calcolare
+    enum tipo_calcolo_vento metodo_calcolo_vento;
+    enum tipo_calcolo_temperatura metodo_calcolo_temperatura;
+    enum tipo_calcolo_densita metodo_calcolo_densita;
+
+    //Chiedo quali metodi di calcolo vuole utilizzare per ognuno dei 3 valori da calcolare
 
     //Calcolo tutti i parametri a partire dai dati weather
     for(p = in; p != NULL; p = p->prev) {
 
         //calcolo i 3 parametri
-        vento = calcolo_vel_vento(0, h->h_vel1, in->velocita_vento1, h->h_vel2, in->velocita_vento2, in->rugosita, altezza_mozzo);
-        temperatura = calcolo_temperatura_aria(h->h_t1, in->temperatura1, h->h_t2, in->temperatura2, altezza_mozzo);
-        densita = calcolo_densita_aria();  
+        vento = calcolo_vel_vento(metodo_calcolo_vento, h->h_vel1, in->velocita_vento1, h->h_vel2, in->velocita_vento2, in->rugosita, ,altezza_mozzo);
+        temperatura = calcolo_temperatura_aria(metodo_calcolo_temperatura, h->h_t1, in->temperatura1, h->h_t2, in->temperatura2, altezza_mozzo);
+        densita = calcolo_densita_aria(metodo_calcolo_densita, h->h_pressione, in->pressione, , temperatura,altezza_mozzo);  
         
         //salvo i 3 parametri calcolati nell'elemento corrente
         out = aggiungi_elemento(out, vento, temperatura, densita);
