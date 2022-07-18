430675cdb272fbd593c868ba8972385512c41838
Edoardo Menegotto <edoardo.menegotto@studenti.unipd.it>
2022-07-18 15:26:08 +0200

    inserimento metodi di calcolo per parametri

--- lib/parametri/include/parametri.h
+++ lib/parametri/include/parametri.h
@@ -18,6 +18,14 @@
         enum tipo_calcolo_densita densita;
     }
 
-    
+    struct parametro *aggiungi_elemento(const struct parametro *elemento_attuale, float val_vento, float val_temperatura, float val_densita);
+
+    float calcolo_vel_vento(enum tipo_calcolo_vento metodo, float altezza1, float velocita1, float altezza2, float velocita2, float rugosita, float h_ostacolo,float altezza_x);
+
+    float calcolo_temperatura_aria(enum tipo_calcolo_temperatura metodo, float altezza1, float temperatura1, float altezza2, float temperatura2, float altezza_x);
+
+    float calcolo_densita_aria(enum tipo_calcolo_densita metodo, float altezza1, float pressione1,float temperatura_x,float altezza_x);
+
+
     
 #endif
