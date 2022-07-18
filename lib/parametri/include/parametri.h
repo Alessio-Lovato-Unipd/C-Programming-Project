b7332d1bf4808fc19e0ec36819bddcca3d17a06a
Edoardo Menegotto <edoardo.menegotto@studenti.unipd.it>
2022-07-18 15:56:27 +0200

    eliminazione lista parametri uscita

--- lib/parametri/include/parametri.h
+++ lib/parametri/include/parametri.h
@@ -6,10 +6,8 @@
     typedef enum tipo_calcolo_densita {BAROMETRICO, GAS_IDEALE};
 
     struct parametro{
-        float velocita_vento;
-        float temperatura_aria;
-        float densita_aria;
-        struct parametro *next;
+        float vento_mozzo;
+        float densita_aria_mozzo;
     };
     
     struct tipo_metodo{
@@ -17,8 +15,6 @@
         enum tipo_calcolo_temperatura temperatura;
         enum tipo_calcolo_densita densita;
     }
-
-    struct parametro *aggiungi_elemento(const struct parametro *elemento_attuale, float val_vento, float val_temperatura, float val_densita);
 
     float calcolo_vel_vento(enum tipo_calcolo_vento metodo, float altezza1, float velocita1, float altezza2, float velocita2, float rugosita, float h_ostacolo,float altezza_x);
 
--- lib/parametri/src/parametri.c
+++ lib/parametri/src/parametri.c
@@ -3,27 +3,6 @@
 #include "formule.h"
 #include "estrattore_csv.h"
 
-/************************GESTIONE STRUTTURA PARAMETRI********************/
-
-struct parametro *aggiungi_elemento(const struct parametro *elemento_attuale, float val_vento, float val_temperatura, float val_densita)
-{
-    struct parametro *nuovo_elemento;
-
-    nuovo_elemento = malloc(sizeof(struct parametro));
-    if (nuovo_elemento == NULL) {
-        printf("Error: malloc() failed in insert()\n");
-		exit(EXIT_FAILURE);
-    }
-
-    //salvo i dati
-    nuovo_elemento->velocita_vento = val_vento;
-    nuovo_elemento->temperatura_aria = val_temperatura;
-    nuovo_elemento->densita_aria = val_densita;
-    //salvo posizione 
-    nuovo_elemento->next = elemento_attuale; 
-
-    return nuovo_elemento;
-}
 
 /**************************GESTIONE DEI CALCOLI*****************/
 float calcolo_vel_vento(enum tipo_calcolo_vento metodo, float altezza1, float velocita1, float altezza2, float velocita2, float rugosita, float h_ostacolo,float altezza_x)
@@ -91,22 +70,16 @@ float calcolo_densita_aria(enum tipo_calcolo_densita metodo, float altezza1, flo
 
 
 /******************CALCOLO DEI PARAMETRI******************/
-void calcolo_parametri(const struct weather *in, const struct altezze  *h, const struct tipo_metodo *metodo, float altezza_mozzo, struct parametro *out)
+struct parametro calcolo_parametri(const struct weather *in, const struct altezze  *h, const struct tipo_metodo *metodo, float altezza_mozzo)
 {
-    struct weather *p;    
+    struct parametro out;    
     float vento, temperatura, pressione, densita;
     float altezza_ostacolo;
 
-    //Calcolo tutti i parametri a partire dai dati weather
-    for(p = in; p != NULL; p = p->prev) {
-
-        //calcolo i 3 parametri
-        vento = calcolo_vel_vento(metodo->vento, h->h_vel1, in->velocita_vento1, h->h_vel2, in->velocita_vento2, in->rugosita, altezza_ostacolo,altezza_mozzo);
-        temperatura = calcolo_temperatura_aria(metodo->temperatura, h->h_t1, in->temperatura1, h->h_t2, in->temperatura2, altezza_mozzo);
-        densita = calcolo_densita_aria(metodo->densita, h->h_pressione, in->pressione, temperatura,altezza_mozzo);  
-        
-        //salvo i 3 parametri calcolati nell'elemento corrente
-        out = aggiungi_elemento(out, vento, temperatura, densita);
-    }
+    //calcolo i 3 parametri
+    out.vento_mozzo = calcolo_vel_vento(metodo->vento, h->h_vel1, in->velocita_vento1, h->h_vel2, in->velocita_vento2, in->rugosita, altezza_ostacolo,altezza_mozzo);
+    temperatura = calcolo_temperatura_aria(metodo->temperatura, h->h_t1, in->temperatura1, h->h_t2, in->temperatura2, altezza_mozzo);
+    out.densita_aria_mozzo = calcolo_densita_aria(metodo->densita, h->h_pressione, in->pressione, temperatura,altezza_mozzo);  
     
+    return out;
 }
