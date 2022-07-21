3a189df430b3b5fed6607640abb87383ac1da24f
Edoardo Menegotto <edoardo.menegotto@studenti.unipd.it>
2022-07-21 19:21:51 +0200

    modifica dati dei test

--- test/test_formule/test_formule.c
+++ test/test_formule/test_formule.c
@@ -16,19 +16,19 @@ void test_interpolazione_lineare()
 void test_interpolazione_logaritmica()
 {
     float x = interpolazione_logaritmica(10, 5.32, 80, 7.81, 20);
-    TEST_ASSERT_FLOAT_WITHIN(1, 6.15, x);
+    TEST_ASSERT_EQUAL_FLOAT(6.15, x);
 }
 
 void test_profilo_logaritmico()
 {
     float x = profilo_logaritmico(10, 5.32, 0.15, 5, 20);
-    TEST_ASSERT_EQUAL_FLOAT(6.63, x);
+    TEST_ASSERT_FLOAT_WITHIN(0.001, 6.63494, x);
 }
 
 void test_hellman()
 {
     float x = hellman(10, 5.32, 0.15, 20);
-    TEST_ASSERT_EQUAL_FLOAT(6.129, x);
+    TEST_ASSERT_FLOAT_WITHIN(0.001, 6.129655, x);
 }
 
 void test_gradiente_lineare()
@@ -46,13 +46,13 @@ void test_calcolo_pressione()
 void test_barometrico()
 {
     float x = barometrico(0, 98405.7, 267.6, 20);
-    TEST_ASSERT_EQUAL_FLOAT(1.2775, x);
+    TEST_ASSERT_FLOAT_WITHIN(0.01, 1.277, x);
 }
 
 void test_gas_ideale()
 {
-    float x = gas_ideale(0, 98405.7, 6.15, 20);
-    TEST_ASSERT_EQUAL_FLOAT(1.2777, x);
+    float x = gas_ideale(0, 98405.7, 267.6, 20);
+    TEST_ASSERT_FLOAT_WITHIN(0.01, 1.277, x);
 }
 
 
