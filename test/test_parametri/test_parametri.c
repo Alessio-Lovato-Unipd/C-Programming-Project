7577f9e8408680dcaec518c5a2bb5d1441287354
Edoardo Menegotto <edoardo.menegotto@studenti.unipd.it>
2022-07-22 17:13:18 +0200

    implementazion test parametri

--- test/test_parametri/test_parametri.c
+++ test/test_parametri/test_parametri.c
@@ -4,26 +4,26 @@
 
 void test_calcolo_vento_int_lin()
 {
-    float i = calcolo_vel_vento(INTERPOLAZIONE_LINEARE_V, 10, 5.32697, 80, 7.80697, 0, 0, 30);
-    TEST_ASSERT_EQUAL_FLOAT(34, i);
+    float i = calcolo_vel_vento(INTERPOLAZIONE_LINEARE_V, 10, 5.32697, 80, 7.80697, 0.15, 5, 20);
+    TEST_ASSERT_FLOAT_WITHIN(0.1, 5.6812557, i);
 }
 
 void test_calcolo_vento_int_log()
 {
     float x = calcolo_vel_vento(INTERPOLAZIONE_LOGARITMICA, 10, 5.32697, 80, 7.80697, 0.15, 5, 20);
-    TEST_ASSERT_EQUAL_FLOAT(6.15, x);
+    TEST_ASSERT_FLOAT_WITHIN(0.01, 6.15, x);
 }
 
 void test_calcolo_vento_profilo_logaritmico()
 {
     float x = calcolo_vel_vento(PROFILO_LOGARITMICO, 10, 5.32697, 80, 7.80697, 0.15, 5, 20);
-    TEST_ASSERT_FLOAT_WITHIN(0.001, 6.63494, x);
+    TEST_ASSERT_FLOAT_WITHIN(0.01, 6.63494, x);
 }
 
 void test_calcolo_vento_hellmann()
 {
-    float x = calcolo_vel_vento(PROFILO_LOGARITMICO, 10, 5.32697, 80, 7.80697, 0.15, 5, 20);
-    TEST_ASSERT_FLOAT_WITHIN(0.001, 6.129655, x);
+    float x = calcolo_vel_vento(HELLMAN, 10, 5.32697, 80, 7.80697, 0.15, 5, 20);
+    TEST_ASSERT_FLOAT_WITHIN(0.01, 6.129655, x);
 }
 
 void test_calcolo_temperatura_int_lin()
@@ -50,6 +50,26 @@ void test_calcolo_densita_gas_ideale()
     TEST_ASSERT_FLOAT_WITHIN(0.01, 1.277, x);
 }
 
+void test_aggiungi_elemento()
+{
+    struct parametro *p = NULL;
+    p = aggiungi_elemento(p, 5, 3);
+    TEST_ASSERT((p->vento == 5) && (p->densita_aria == 3));
+}
+
+void test_calcolo_parametri()
+{
+    int errore = 0;
+	struct dati_weather *puntatore_dati_weather = NULL;
+    struct tipo_metodo metodi_calcolo = {INTERPOLAZIONE_LOGARITMICA, INTERPOLAZIONE_LINEARE_T, GAS_IDEALE};
+    struct parametro *head_uscita = NULL;
+	puntatore_dati_weather = estrazione_dati_weather(puntatore_dati_weather, "../../../data/weather.csv", &errore);
+    head_uscita = calcolo_parametri(puntatore_dati_weather, &metodi_calcolo, 5, 20, head_uscita);
+
+    TEST_ASSERT_FLOAT_WITHIN(0.01, 6.15, head_uscita->vento);
+    TEST_ASSERT_FLOAT_WITHIN(0.01, 1.277, head_uscita->densita_aria);
+}
+
 int main()
 {
     UNITY_BEGIN();
@@ -64,7 +84,10 @@ int main()
     RUN_TEST(test_calcolo_densita_barometrico);
     RUN_TEST(test_calcolo_densita_gas_ideale);
 
-    
+    RUN_TEST(test_aggiungi_elemento);  
+
+    RUN_TEST(test_calcolo_parametri);  
+       
 
     return UNITY_END();
 }
