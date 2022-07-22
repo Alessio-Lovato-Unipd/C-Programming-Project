5adbea71d53b49fc6f3cc30852237e0b38480f8f
Edoardo Menegotto <edoardo.menegotto@studenti.unipd.it>
2022-07-22 12:01:24 +0200

    aggiunta test parametri su tipologie di calcolo

--- test/test_parametri/test_parametri.c
+++ test/test_parametri/test_parametri.c
@@ -2,17 +2,69 @@
 #include "parametri.h"
 
 
-void test_calcolo_vento()
+void test_calcolo_vento_int_lin()
 {
     float i = calcolo_vel_vento(INTERPOLAZIONE_LINEARE_V, 10, 5.32697, 80, 7.80697, 0, 0, 30);
     TEST_ASSERT_EQUAL_FLOAT(34, i);
+}
+
+void test_calcolo_vento_int_log()
+{
+    float x = calcolo_vel_vento(INTERPOLAZIONE_LOGARITMICA, 10, 5.32697, 80, 7.80697, 0.15, 5, 20);
+    TEST_ASSERT_EQUAL_FLOAT(6.15, x);
+}
+
+void test_calcolo_vento_profilo_logaritmico()
+{
+    float x = calcolo_vel_vento(PROFILO_LOGARITMICO, 10, 5.32697, 80, 7.80697, 0.15, 5, 20);
+    TEST_ASSERT_FLOAT_WITHIN(0.001, 6.63494, x);
+}
+
+void test_calcolo_vento_hellmann()
+{
+    float x = calcolo_vel_vento(PROFILO_LOGARITMICO, 10, 5.32697, 80, 7.80697, 0.15, 5, 20);
+    TEST_ASSERT_FLOAT_WITHIN(0.001, 6.129655, x);
+}
+
+void test_calcolo_temperatura_int_lin()
+{
+    float x = calcolo_temperatura_aria(INTERPOLAZIONE_LINEARE_T, 2, 267.6, 10, 267.57, 20);
+    TEST_ASSERT_FLOAT_WITHIN(0.01, 267.5325, x);
+}
+
+void test_calcolo_temperatura_gradiente()
+{
+    float x = calcolo_temperatura_aria(GRADIENTE_LINEARE, 2, 267.6, 10, 267.57, 20);
+    TEST_ASSERT_EQUAL_FLOAT(267.483, x);
+}
+
+void test_calcolo_densita_barometrico()
+{
+    float x = calcolo_densita_aria(BAROMETRICO, 0, 98405.7, 267.6, 20);
+    TEST_ASSERT_FLOAT_WITHIN(0.01, 1.277, x);
+}
+
+void test_calcolo_densita_gas_ideale()
+{
+    float x = calcolo_densita_aria(GAS_IDEALE, 0, 98405.7, 267.6, 20);
+    TEST_ASSERT_FLOAT_WITHIN(0.01, 1.277, x);
 }
 
 int main()
 {
     UNITY_BEGIN();
 
-    RUN_TEST(test_calcolo_vento);
+    //test delle varie possibilità di calcolo dei parametri
+    RUN_TEST(test_calcolo_vento_int_lin);
+    RUN_TEST(test_calcolo_vento_int_log);
+    RUN_TEST(test_calcolo_vento_profilo_logaritmico);
+    RUN_TEST(test_calcolo_vento_hellmann);
+    RUN_TEST(test_calcolo_temperatura_int_lin);
+    RUN_TEST(test_calcolo_temperatura_gradiente);
+    RUN_TEST(test_calcolo_densita_barometrico);
+    RUN_TEST(test_calcolo_densita_gas_ideale);
+
+    
 
     return UNITY_END();
 }
