85f6f7d8b37a8221c7da78e62712dc09ad48003c
Alessio Lovato <alessio.lovato.1@studenti.unipd.it>
2022-07-24 13:03:58 +0200

    Non salva più elementi quando l'altezza dell'hub (nel caso di duplicati) non è presente.

--- lib/estrazione_csv/src/estrattore_csv.c
+++ lib/estrazione_csv/src/estrattore_csv.c
@@ -20,6 +20,8 @@ struct turbina *estrazione_dati_turbine(struct turbina *puntatore, char *percors
         printf("trova nel percorso \"../../data/turbine_data.csv\" rispetto a dove è stato lanciato l'eseguibile\n\n");
         return(NULL);
     }
+
+	//fare ciclo for con #define RIGHE_DA_SALTARE?
 	*errore=csv_read_record(&file, &fields); //salto l'intestazione del file csv
 	if(*errore != CSV_OK){
 		return NULL;
@@ -29,10 +31,13 @@ struct turbina *estrazione_dati_turbine(struct turbina *puntatore, char *percors
 	if(*errore != CSV_OK){
 		return NULL;
 	}
+	//fine possibile ciclo for
 
-
+	int num_riga = 3;
 	while ((*errore = csv_read_record(&file, &fields)) == CSV_OK) {
         puntatore = nuovo_elemento_turbina(puntatore, fields, false , strchr(fields[8], ';'));
+		printf("Riga: %d\n",num_riga);
+		num_riga++;
     }
 
     if (*errore == CSV_END) {
@@ -125,12 +130,27 @@ struct turbina *nuovo_elemento_turbina(struct turbina *elemento_attuale_turbina,
 			if(punto_virgola_temp != NULL)
 				punto_virgola_temp = punto_virgola;
 			elemento_attuale_turbina=nuovo_elemento_turbina(elemento_attuale_turbina, fields, true, punto_virgola);
+			if(elemento_attuale_turbina->id == NULL)
+			{
+				//un valore di altezza era nullo e ha reso l'id nullo, quindi elimino il nodo della lista
+				struct turbina *elemento_successivo = elemento_attuale_turbina->prev;
+				free(elemento_attuale_turbina->nome);
+				free(elemento_attuale_turbina->wind_speed);
+				free(elemento_attuale_turbina->id);
+				if(elemento_attuale_turbina->power_coefficients != NULL)
+					free(elemento_attuale_turbina->power_coefficients);
+				if(elemento_attuale_turbina->power_curves != NULL)
+					free(elemento_attuale_turbina->power_curves);
+				free(elemento_attuale_turbina);
+				elemento_attuale_turbina = elemento_successivo;
+			}
 		}
 
 	}
-	
+
     //salvo posizione elemento precedente
     nuova->prev = elemento_attuale_turbina;
+
     return nuova;
    
 }
@@ -149,6 +169,7 @@ void svuota_lista_turbine_data(struct turbina *head_turbina)
         temporaneo_turbina = head_turbina->prev;
         free(head_turbina->nome);
 		free(head_turbina->wind_speed);
+		free(head_turbina->id);
 		if(head_turbina->power_coefficients != NULL)
 			free(head_turbina->power_coefficients);
 		if(head_turbina->power_curves != NULL)
