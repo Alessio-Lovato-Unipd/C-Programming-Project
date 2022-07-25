b75d06b7523628ab8a04b513c6a8148b836c566a
Edoardo Menegotto <edoardo.menegotto@studenti.unipd.it>
2022-07-25 12:45:25 +0200

    Implementazione funzioni stampa curve di potenza e coefficinti di potenza

--- lib/plot/include/plot.h
+++ lib/plot/include/plot.h
@@ -4,8 +4,10 @@
     #include <stdlib.h>
     #include <stdio.h>
     #include "estrattore_csv.h"
-    #include "../../external/gnuplot_i/src/gnuplot_i.h"
+    #include "../../../external/gnuplot_i/src/gnuplot_i.h"
 
     void plot_curva_potenza(const struct turbina *turbina);
 
+    void plot_curva_coefficienti(const struct turbina *turbina);
+    
 #endif
--- lib/plot/src/plot.c
+++ lib/plot/src/plot.c
@@ -1,16 +1,19 @@
 #include "plot.h"
 
+//gnuplot_plot genera un file coi dati numerici anche?
+//altrimenti genero il file con gnuplot_write e poi lo plotto con gnuplot_atmpfile
+
+
 void plot_curva_potenza(const struct turbina *turbina)
 {
     gnuplot_ctrl *gp = NULL;  
     gp = gnuplot_init();
-
     gnuplot_setstyle(gp, "linespoints");
     gnuplot_set_xlabel(gp, "Velocità del vento [m/s]");
     gnuplot_set_ylabel(gp, "Potenza [kW]");
     gnuplot_cmd(gp, "set grid back nopolar");
     gnuplot_cmd(gp, "xtics nomxtics ytics nomytics noztics nomztics nortics nomrtics nox2tics nomx2tics noy2tics nomy2tics nocbtics nomcbtics");
-    gnuplot_plot_xy(gp, turbina->wind_speed, turbina->power_curves, LUNGHEZZA_VETTORE_POWER_CURVES, "Curva di Potenza");
+    gnuplot_plot_xy(gp, (double*)turbina->wind_speed, (double *)turbina->power_curves, LUNGHEZZA_VETTORE_POWER_CURVES, "Curva di Potenza");
 
     gnuplot_close(gp);
 
@@ -22,17 +25,23 @@ void plot_curva_coefficienti(const struct turbina *turbina)
     gp = gnuplot_init();
 
     gnuplot_setstyle(gp, "linespoints");
+    gnuplot_set_xlabel(gp, "Velocità del vento [m/s]");
+    gnuplot_set_ylabel(gp, "Coefficienti di potenza");
+    gnuplot_plot_xy(gp, (double*)turbina->wind_speed, (double*)turbina->power_coefficients, LUNGHEZZA_VETTORE_POWER_COEFFICIENT, "Curva coefficienti di potenza");
 
     gnuplot_close(gp);
 }
 
-void plot_potenza(const struct turbina *turbina)//scrivo anche un file csv coi dati calcolati
+/*
+void plot_potenza(const struct turbina *turbina, const struct weather *meteo, int lunghezza_vettore)//scrivo anche un file csv coi dati calcolati
 {
     gnuplot_ctrl *gp = NULL;  
     gp = gnuplot_init();
 
     gnuplot_setstyle(gp, "linespoints");
-    gnuplot_write_xy_csv(, turbina->wind_speed, turbina->potenza_nominale, , );
+    gnuplot_write_xy_csv("potenza.csv", (double*), (double*)turbina->potenza_nominale, lunghezza_vettore, "#Potenza elettrica generata dalla turbina nel tempo");
 
     gnuplot_close(gp);
 }
+
+*/
