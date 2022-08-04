0eb34c5004e3eda068e5a85744520d95a83288bb
Alberto Piccina <alberto.piccina@studenti.unipd.it>
2022-08-04 17:17:44 +0200

    Correzione errori (non tutti) risultanti dalla compilazione del programma.

--- app/include/main.h
+++ app/include/main.h
@@ -6,8 +6,12 @@
     #include <stdlib.h>
     #include <stdbool.h>
     #include "../../lib/estrazione_csv/include/estrattore_csv.h"
+    #include "../../lib/formule/include/formule.h"
+    #include "../../lib/parametri/include/parametri.h"
+    #include "../../lib/calcolo_output/include/calcolo_output.h"
 
     #define PERCORSO_TURBINE_DATA "../../data/turbine_data.csv"
+    #define PERCORSO_WEATHER "../../data/weather.csv"
     #define PERCORSO_POWER_COEFFICIENT "../../data/power_coefficient_curves.csv"
 	#define PERCORSO_POWER_CURVES "../../data/power_curves.csv"
     #define VALORE_ARGOMENTI_INSERIBILI 8
--- app/src/main.c
+++ app/src/main.c
@@ -27,9 +27,12 @@ int main(int argc, char *argv[])
     //fine generazione lista
 
     //inizio generazione lista dati meteorologici tramite la lettura da file
-    dati = apertura_file_weather(&file, dati, PERCORSO_WEATHER, &errore);
+    struct csv file;
+    char** fields=NULL;
+
+    dati = apertura_file_weather(&file, fields, dati, PERCORSO_WEATHER, &errore);
     dati=estrazione_dati_weather(dati, PERCORSO_WEATHER, &errore);
-    if (errore!=CSV_IO)
+    if (errore!=CSV_OK)
     {
         return(EXIT_FAILURE);
     }
@@ -60,6 +63,7 @@ int main(int argc, char *argv[])
 
         //calcolo dei parametri a partire dai dati meteorologici
         struct parametro *parametri=NULL;
+        struct parametro *head_parametri;
         float altezza_ostacolo=0;
 
         parametri = malloc(sizeof(struct parametro));
@@ -69,7 +73,7 @@ int main(int argc, char *argv[])
         }
     
         altezza_ostacolo=atof(argv[7]);
-        parametri=calcolo_parametri(dati, metodo_calcolo_parametri, altezza_ostacolo, turbina_cercata->altezza_mozzo, parametri);
+        parametri=calcolo_parametri(dati, metodo_calcolo_parametri, altezza_ostacolo, turbina_cercata->altezza_mozzo, head_parametri);
         //fine calcolo parametri
 
         float potenza_in_uscita=0;
@@ -96,163 +100,3 @@ int main(int argc, char *argv[])
 }
 
 //RICODATI DI DEALLOCARE LA MEMORIA!!!!!!
-
-//--------------------------------------------------
-/*
-int main()
-{
-    #if(tipo_dati_stampa==0)
-
-    struct turbina *head_turbina=NULL; //definisco lista per il salvataggio delle turbine
-    struct turbina *temporaneo=NULL; //variabile temporanea per dimostrazione stampa <-------------------- DA ELIMINARE
-	//struct csv file_coefficient;
-	//struct csv file_power;
-    bool penultimo=false; //variabile temporanea per dimostrazione stampa <------------------------------- DA ELIMINARE
-    bool ultimo=false; //variabile temporanea per dimostrazione stampa <---------------------------------- DA ELIMINARE
-    int errore=0;
-	
-    //generazione lista con lettura da file
-    head_turbina=estrazione_dati_turbine(head_turbina, PERCORSO_TURBINE_DATA, &errore);
-    if (errore==CSV_E_IO)
-    {
-        return(EXIT_FAILURE);
-    }
-	//reading_file_power_coefficient(&file_coefficient, head_turbina, PERCORSO_POWER_COEFFICIENT, &errore);
-	//reading_file_power_curves(&file_power, head_turbina, PERCORSO_POWER_CURVES, &errore);
-    // fine generazione lista
-
-    //stampa un elemento della lista            <------------------------------------------------------INIZIO CODICE DA ELIMINARE (ESEMPIO)
-    temporaneo = cerca_dati_turbina("AD116/5000", 0.0, head_turbina);
-    if (temporaneo == NULL)
-    {
-        printf("Modello turbina non trovato!\n\n\n");
-    }else{
-        printf("Ricerca modello: %s\n", temporaneo->nome);
-        printf("Potenza nominale: %d\n\n\n", temporaneo->potenza_nominale);
-		
-    }
-    //fine stampa elemento lista
-
-    //esempio stampa lista per verifica turbine_data.csv         
-    temporaneo=head_turbina;
-    printf("*****   Stampa elementi lista   *****\n\n");
-    while (!ultimo) {
-        head_turbina=conversione_dati_in_booleano(temporaneo);
-        printf(" Modello turbina: %s\n", temporaneo->nome);
-        printf(" ID: %s\n", temporaneo->id);
-        printf(" Potenza nominale: %d\n", temporaneo->potenza_nominale);
-        printf(" Diametro del rotore: %d\n", temporaneo->diametro_rotore);
-        printf(" Altezza del mozzo: %f\n", temporaneo->altezza_mozzo);
-        printf(" Char power_coefficient: %s\n", temporaneo->char_p_coefficient);
-        printf(" Bool power_coefficient: %d\n", temporaneo->bool_p_coefficient);
-        printf(" Char power_curves: %s\n", temporaneo->char_p_curves);
-        printf(" Bool power_curves: %d\n", temporaneo->bool_p_curves);
-		printf(" Velocità vento: \n\n\n");
-/*	for(int i = 0; i < (NUMERO_COLONNE_POWER_COEFFICIENT_CURVES- 1); i++)
-			printf("%f\t", temporaneo->wind_speed[i]);
-		printf("\n");
-		printf("Coefficienti di potenza: \n");
-		if(temporaneo->power_coefficients != NULL){
-			for(int i = 0; i < (NUMERO_COLONNE_POWER_COEFFICIENT_CURVES - 1); i++)
-				printf("%f\t", temporaneo->power_coefficients[i]);
-		}
-		printf("\n");
-		printf("Curva di potenza: \n");
-		if(temporaneo->power_curves != NULL){
-			for(int i = 0; i < (NUMERO_COLONNE_POWER_CURVES - 1); i++)
-				printf("%d\t", temporaneo->power_curves[i]);
-		}
-		printf("\n");
-        printf("----\n\n");  
-
-        temporaneo=temporaneo->prev;
-
-        if (temporaneo == NULL)
-        {
-            penultimo=true;
-        }
-
-        if (penultimo)
-        {
-            ultimo=true;
-        }
-    }
-    //fine esempio di stampa                <------------------------------------------------------------ FINE CODICE DA ELIMINARE
-
-
-    svuota_lista_turbine_data(head_turbina); //deallocazione memoria heap
-
-    return 0;
-
-    #endif
-
-    
-    
-#if(tipo_dati_stampa==1)
-
-    struct weather *temporaneo=NULL; //variabile temporanea per dimostrazione stampa <-------------------- DA ELIMINARE
-    struct dati_weather *dati = NULL;
-    int errore=0;
-
-    dati = apertura_file_weather(&file, dati, PERCORSO_WEATHER, &errore);
-	
-	bool penultimo=false; //variabile temporanea per dimostrazione stampa <------------------------------- DA ELIMINARE
-    bool ultimo=false; //variabile temporanea per dimostrazione stampa <---------------------------------- DA ELIMINARE
-
-    dati=estrazione_dati_weather(dati, PERCORSO_WEATHER, &errore);
-    if (errore!=CSV_E_IO)
-    {
-        return(EXIT_FAILURE);
-    }
-	controllo_csv(&errore);
-
-    //stampa un elemento della lista            <------------------------------------------------------INIZIO CODICE DA ELIMINARE (ESEMPIO)
-    temporaneo = cerca_dati_weather("2010-01-01 09:00:00+01:00",dati->head_weather);
-    if (temporaneo == NULL)
-    {
-        printf("Giorno e ora non trovati!\n\n\n");
-    }else{
-        printf("Ordine temporale del dato: %s\n", temporaneo->orario);
-        printf("Pressione: %f\n", temporaneo->pressione);
-        printf("Temperatura ad altezza1: %f\n", temporaneo->temperatura1);
-        printf("Velocita' del vento ad altezza1: %f\n", temporaneo->velocita_vento1);
-        printf("Rugosita': %f\n", temporaneo->rugosita);
-        printf("Temperatura ad altezza2: %f\n", temporaneo->temperatura2);
-        printf("Velocita' del vento ad altezza2: %f\n", temporaneo->velocita_vento2);
-    }
-    //fine stampa elemento lista
-
-    //esempio stampa lista per verifica turbine_data.csv         
-    temporaneo=dati->head_weather;
-    printf("*****   Stampa elementi lista   *****\n\n");
-    while (!ultimo) {
-        printf("Ordine temporale del dato: %s\n", temporaneo->orario);
-        printf("Pressione: %f\n", temporaneo->pressione);
-        printf("Temperatura ad altezza1: %f\n", temporaneo->temperatura1);
-        printf("Velocita' del vento ad altezza1: %f\n", temporaneo->velocita_vento1);
-        printf("Rugosita': %f\n", temporaneo->rugosita);
-        printf("Temperatura ad altezza2: %f\n", temporaneo->temperatura2);
-        printf("Velocita' del vento ad altezza2: %f\n", temporaneo->velocita_vento2);
-        printf("----\n\n");
-
-        temporaneo=temporaneo->prev;
-
-        if (temporaneo == NULL)
-        {
-            penultimo=true;
-        }
-
-        if (penultimo)
-        {
-            ultimo=true;
-        }
-    }
-    //fine esempio di stampa                <------------------------------------------------------------ FINE CODICE DA ELIMINARE
-
-    svuota_dati_weather(dati); //deallocazione memoria heap
-
-    return 0;
-
-    #endif
-
-}*/
