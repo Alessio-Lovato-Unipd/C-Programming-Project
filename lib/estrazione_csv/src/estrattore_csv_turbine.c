5c5ac18c70e05d15fcde580bee350f8748586305
Alessio Lovato <alessio.lovato.1@studenti.unipd.it>
2022-08-04 11:16:22 +0200

    Durante un meeting è stato risolto il problema delle funzioni di svuotamento liste nei file 'estrattore_csv_weather.c' e 'estrattore_csv_turbine.c'. Ora le due funzioni sono di tipo struct delle rispettive variabili.

--- lib/estrazione_csv/src/estrattore_csv_turbine.c
+++ lib/estrazione_csv/src/estrattore_csv_turbine.c
@@ -148,12 +148,12 @@ struct turbina *nuovo_elemento_turbina(struct turbina *elemento_attuale_turbina,
    
 }
 
-void svuota_lista_turbine_data(struct turbina *head_turbina)
+struct turbina *svuota_lista_turbine_data(struct turbina *head_turbina)
 {
     struct turbina *temporaneo_turbina = head_turbina;
 	
 	if(head_turbina == NULL){
-		return;
+		return NULL;
 	}
 	
     do
@@ -163,8 +163,8 @@ void svuota_lista_turbine_data(struct turbina *head_turbina)
         head_turbina = temporaneo_turbina;
 
     }while(temporaneo_turbina!=NULL);
-    head_turbina=NULL;
-	return;
+
+	return head_turbina;
 }
 
 void elimina_nodo_turbina (struct turbina *nodo)
