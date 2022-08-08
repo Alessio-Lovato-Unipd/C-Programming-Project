c389c343376431e88e414b100938277ec665aee5
Matteo Civiero <matteo.civiero@studenti.unipd.it>
2022-08-08 14:12:52 +0200

    ingresso di data e ora di analisi per argomento e non per codice

--- app/include/potenze_giorno.h
+++ app/include/potenze_giorno.h
@@ -14,8 +14,7 @@
     #define PERCORSO_WEATHER "../../data/weather.csv"
     #define PERCORSO_POWER_COEFFICIENT "../../data/power_coefficient_curves.csv"
 	#define PERCORSO_POWER_CURVES "../../data/power_curves.csv"
-	#define DATA_SCELTA "2010-01-01 04:00:00+01:00" /*-------------------->MODIFICA DATA PER SELEZIONARE GIORNO DA ANALIZZARE<---------------------------------*/
-    #define VALORE_ARGOMENTI_INSERIBILI 9
+    #define VALORE_ARGOMENTI_INSERIBILI 10
 
     /*
     -----ARGOMENTI DA PASSARE AL PROGRAMMA MAIN-----
@@ -29,6 +28,7 @@
         - argv[6] ---> {INTERPOLAZIONE_LINEARE_O, INTERPOLAZIONE_LOGARITMICA_O}, per la scelta del tipo di interpolazione da utilizzare per il calcolo dell'output
         - argv[7] ---> valore di altezza_ostacolo, mettere 0 se si pensa di non utilizzare PROFILO_LOGARITMICO in argv[2]
         - argv[8] ---> altezza del mozzo considerata
+		- argv[9] ---> data e orario di inizio analisi dati, inserire tra le virgolette ""
 
         Esempio: ./main E-126/7500 INTERPOLAZIONE_LINEARE_V INTERPOLAZIONE_LINEARE_T BAROMETRICO CURVE_DI_POTENZA INTERPOLAZIONE_LINEARE_O 1
     */
